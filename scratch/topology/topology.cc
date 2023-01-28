a9b1c128de8c941e7eeb6b8303d74e46f6ff6d6e
DarkKnight <aliranjbari86@gmail.com>
2023-01-29 00:39:54 +0330

    build the connections

--- scratch/topology/topology.cc
+++ scratch/topology/topology.cc
@@ -7,21 +7,92 @@
 
     * all the connections use 802.11 standard
 
-    (sender)---->|            |----->(receiver)
-    (sender)---->|LoadBalancer|----->(receiver)
-    (sender)---->|____________|----->(receiver)
+    (sender)-*                          *-(receiver)
+    (sender)-*    *-(LoadBalancer)-*    *-(receiver)
+    (sender)-*                          *-(receiver)
 
 */
-
 #include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/wifi-module.h"
+#include "load-balancer.h"
 
 using namespace ns3;
 
+int main(int argc, char *argv[]) {
 
-int
-main(int argc, char* argv[]) {
+	// Create three sender and three receivers
+	NodeContainer senderNodes;
+	NodeContainer receiverNodes;
+	senderNodes.Create(3);
+	receiverNodes.Create(3);
 
-    NS_LOG_UNCOND ("A simple topology\n");
-    CommandLine cmd;
-    cmd.Parse (argc, argv);
+	// Create load balancer node and add LB applicatoin
+	NodeContainer loadBalancerNode;   
+	Ptr<LoadBalancer> loadBalancerApplication = CreateObject<LoadBalancer>();
+	loadBalancerNode.Get (0)->AddApplication (loadBalancerApplication);
+
+
+	YansWifiChannelHelper channel = YansWifiChannelHelper::Default ();
+	YansWifiPhyHelper phy;
+	phy.SetChannel (channel.Create ());
+
+	WifiHelper wifi;
+	wifi.SetRemoteStationManager ("ns3::AarfWifiManager");
+
+  WifiMacHelper mac;
+  Ssid ssid = Ssid ("ns-3-ssid");
+
+  // Create mac for Station nodes
+  mac.SetType ("ns3::StaWifiMac",
+               "Ssid", SsidValue (ssid),
+               "ActiveProbing", BooleanValue (false));
+
+
+  NetDeviceContainer staDeviceSender;
+  NetDeviceContainer staDeviceReceiver;
+  staDeviceSender = wifi.Install (phy, mac, senderNodes);
+  staDeviceReceiver = wifi.Install (phy, mac, receiverNodes);
+
+  // Create mac for Access Point node
+  mac.SetType ("ns3::ApWifiMac",
+               "Ssid", SsidValue(ssid));
+
+  NetDeviceContainer apDeviceLoadBalancer;
+  apDeviceLoadBalancer = wifi.Install (phy, mac, loadBalancerNode);
+
+
+
+  // ..................... we can add mobility here !
+
+
+
+  InternetStackHelper stack;
+  stack.Install (loadBalancerNode);
+  stack.Install (senderNodes);
+  stack.Install (receiverNodes);
+
+
+  Ipv4AddressHelper address;
+  address.SetBase ("10.1.1.0", "255.255.255.0");
+  Ipv4InterfaceContainer loadBalancerInterface;
+  loadBalancerInterface = address.Assign (apDeviceLoadBalancer);
+
+  address.SetBase ("10.1.2.0", "255.255.255.0");
+  Ipv4InterfaceContainer senderInterface;
+  senderInterface = address.Assign (staDeviceSender);
+
+  address.SetBase ("10.1.3.0", "255.255.255.0");
+  Ipv4InterfaceContainer receiverInterface;
+  receiverInterface = address.Assign (staDeviceReceiver);
+
+	
+	// Start the simulation
+	Simulator::Run();
+	Simulator::Destroy();
+
+	return 0;
 }
